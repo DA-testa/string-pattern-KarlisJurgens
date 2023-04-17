c882a71a02fc2d8c4401d6159851225c64b55c4e
Kārlis Jurgens <124631657+KarlisJurgens@users.noreply.github.com>
2023-04-17 12:06:05 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -5,10 +5,9 @@ def read_input():
 
     if choice == "F":
         with open('tests/06.txt', 'r') as file:
-            l1 = file.readline().rstrip()
-            l2 = file.readline().rstrip()
-            F_return = (l1, l2)
-            return F_return
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+            return (pattern, text)
     if choice == "I":
         pattern = input().rstrip()
         text = input().rstrip()
@@ -18,31 +17,32 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    #test
-    p = 31
-    m = len(pattern)
+    p = 11 
+    m= len(pattern)
     n = len(text)
-    p_hash = 0
-    t_hash = 0
-    power = 1
-    occurrences = []
+    
+    p_pow = [1] * (n - m + 1)
 
-    for i in range(m):
-        p_hash += ord(pattern[i]) * power
-        t_hash += ord(text[i]) * power
-        if i < m-1:
-            power = power * p
+    for i in range(1, n - m + 1):
 
-    for i in range(n-m+1):
-        if p_hash == t_hash and pattern == text[i:i+m]:
-            occurrences.append(i)
-        if i < n-m:
-            t_hash = t_hash - ord(text[i]) * power
-            t_hash = t_hash * p + ord(text[i+m])
+        p_pow[i] = p_pow[i - 1] * p
+        
+    p_hash = sum(ord(pattern[i]) * p_pow[m - i - 1] for i in range(m))
+    t_hash = sum(ord(text[i]) * p_pow[m - i - 1] for i in range(m))
+    occ = []
+    
+    for i in range(n - m + 1):
+        if p_hash == t_hash:
+            if pattern == text[i:i+m]:
 
-    return [0] if not occurrences else occurrences
+                occ.append(i)
+                
+        if i < n - m:
+            t_hash = (t_hash - ord(text[i]) * p_pow[m - 1]) * p + ord(text[i + m])
+            
+    return occ
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
 
